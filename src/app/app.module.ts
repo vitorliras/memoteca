d6bf8cffed819d44e3d412ceec6336513c6c792b
vitorliras <vitorlirasti@hotmail.com>
2024-02-23 16:40:25 -0300

    diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
 import { FormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ListarPensamentoComponent } from './components/pensamentos/listar-pensa
     CabecalhoComponent,
     RodapeComponent,
     CriarPensamentosComponent,
-    ListarPensamentoComponent
+    ListarPensamentoComponent,
+    PensamentoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento = {
+    conteudo: 'Angular é baum',
+    autoria:'vit',
+    modelo:'modelo1'
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento():string{
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }
+      return 'pensamento-p'
+  }
+
+}
